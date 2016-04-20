67d8a4fd1956366ffe84aceb4fe6a2a36286bcdd
gufito <david@syloper.com>
2016-04-20 01:08:22 -0300

    ahora funciona

--- jquery-revelador.js
+++ jquery-revelador.js
@@ -1,68 +1,26 @@
-(function($) {
+;(function($) {
 
+	var w = $(window);
 	$.fn.revelador = function(opciones, callback) {
 
-		var w = $(window),
-			O = $.extend({
-				offset 		: 200,
-				loader		: 'loader',
-				imgs		: this
-			}, opciones);
+		var imagenes = $('img');
+		var acargar = [];
+
+		for (var i = 0; i < imagenes.length; i++) { if ( $(imagenes[i]).attr('data-src') !== undefined ){ acargar.push(imagenes[i]) } };
+		
 
 		function revelador(){
-			/*var  t = $(this), 
-				st = w.scrollTop(), 
-				sl = w.scrollLeft(), 
-				wb = st + w.height(),
-
-				it = t.offset().top,
-				ib = it + t.height(),
-
-				 d = t.data('src');
-			
-			if((ib <= wb) && (it >= st)){
-				var img = new Image();
-                img.onload = function(){
-                    t.attr("src", d).css({'opacity':1});
-                };
-                img.src = d;
-			}*/
-			O.imgs.filter(function() {
-				/*var $element = $(this);
-
-			    var window_left = w.scrollLeft();
-			    var window_top = w.scrollTop();
-			    var offset = $element.offset();
-			    var left = offset.left;
-			    var top = offset.top;
-			    var d = $element.data('src');
-
-			    if (top + $element.height() >= window_top &&
-			        top - ($element.data('appear-top-offset') || 0) <= window_top + w.height() &&
-			        left + $element.width() >= window_left &&
-			        left - ($element.data('appear-left-offset') || 0) <= window_left + w.width()) {
-			      
-			      	var img = new Image();
-		            img.onload = function(){
-		                $element.attr("src", d).css({'opacity':1});
-		            };
-		            img.src = d;
-
-
-			    } else {
-			      return false;
-			    }*/
-			    var  t = $(this), 
+			$.each(acargar,function(i,e){
+				var  t = $(e), 
 					st = w.scrollTop(), 
-					sl = w.scrollLeft(), 
 					wb = st + w.height(),
 
 					it = t.offset().top,
 					ib = it + t.height(),
 
 					 d = t.data('src');
-				
-				if((ib <= wb) && (it >= st)){
+
+				if((ib >= st) && (it <= wb)){
 					var img = new Image();
 	                img.onload = function(){
 	                    t.attr("src", d).css({'opacity':1});
@@ -70,11 +28,11 @@
 	                img.src = d;
 				}
 			});
-
-	        if (typeof callback === "function") callback.call(this);
 		}
 
-		revelador();
+
+		w.on('scroll', revelador);
+		//revelador();
 	};
 
 }( jQuery ));
